9e426736cf588edcfa6f4f9d6139ae27fb005371
Baili Convention <46886944+followbylyj@users.noreply.github.com>
2019-08-28 10:36:57 +0800

    已通过

---
+++ 循环6.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cstdlib>
+#include<cstdio> 
+
+using namespace std;
+int n,a;
+
+
+
+int main()
+{
+	cin>>n;
+	
+
+int sum = 0;//变量需要初始化
+
+for ( int i=1;i<=n;i++)
+
+sum=sum+i;
+
+cout<<sum<<endl;
+	
+	
+    	
+    	
+	
+	return 0;
+}
