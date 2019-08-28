3328e03267753ae671a059e403ea4611bf499b4c
Baili Convention <46886944+followbylyj@users.noreply.github.com>
2019-08-28 11:12:06 +0800

    C语言

---
+++ 选择10.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cstdlib>
+#include<cstdio> 
+
+using namespace std;
+int a,b,c,d,e;
+
+
+
+int main()
+{
+	cin>>c;
+	if ((c>60)&&(c<75))
+		cout<<"C"<<endl;
+	else if ((c>76)&&(c<90))
+		cout<<"B"<<endl;
+	else if ((c>91)&&(c<100))
+		cout<<"A"<<endl;
+	else
+		cout<<"BAD"<<endl;
+	
+	
+	
+
+
+    	
+    	
+	
+	return 0;
+}
