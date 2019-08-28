e320eb88e879f8f363b2baedb46cf9ad7c842a29
Baili Convention <46886944+followbylyj@users.noreply.github.com>
2019-08-28 13:08:16 +0800

    Add files via upload

---
+++ 循环/循环9.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int n,a,c;
+int maxn;
+int main()
+
+{
+
+cin>>n; 
+    for(int i=1;i<=n;i++)  
+    {  
+        cin>>a;  
+		if(a % 2 !=0)
+			continue;
+		c = a+10;
+			
+		         
+        	
+        	cout<<c<<" ";
+    } 
+
+
+return 0;
+
+}
+ 
