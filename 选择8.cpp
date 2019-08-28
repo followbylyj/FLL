942f335a3509328b6642268b5056c900de1f5054
Baili Convention <46886944+followbylyj@users.noreply.github.com>
2019-08-28 09:29:04 +0800

    选择8

---
+++ 选择8.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int a,b;
+int caixukun;
+
+int main()
+{
+    cin>>a>>b;
+    caixukun=a>b?a:b;
+    cout<<caixukun<<endl;
+   
+    return 0;
+}
