2597868fddbc944d87bbd77acfbb801e4219200d
Baili Convention <46886944+followbylyj@users.noreply.github.com>
2019-09-21 17:31:44 +0800

    Add files via upload

---
+++ 数组/list 5.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cstring>
+#include<string>
+using namespace std;
+int a[100001],b[1000001]n,x,p,q;
+int main(){
+	cin>>n>>x;
+	for(int i=0;i<=n;i++){
+	
+		cin>>a[i];}
+	for(int j=0;j<=x;j++)
+		cin>>b[j]>>endl;
+	if(b[0]==1)
+		swap (b[]
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
