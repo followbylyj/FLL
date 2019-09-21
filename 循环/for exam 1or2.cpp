fc7c97960f93f5d185696563afaacfbe7d76f582
Baili Convention <46886944+followbylyj@users.noreply.github.com>
2019-09-21 15:21:44 +0800

    Add files via upload

---
+++ 循环/for exam 1or2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int m,k,i;
+int main(){
+	cin>>m;
+	for(i=1;i<=m;i++){
+	
+		
+		for(int j=1;j<=m;j++)
+			cout<<"*";
+		cout<<endl;
+	}
+		
+	
+
+	return 0;
+}
